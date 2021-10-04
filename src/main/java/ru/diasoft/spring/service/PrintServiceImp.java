cce40fe106fa00398916d1c07f0ff04d2fc22e14
Бунегина Люба <lbunegina@diasoft.ru>
2021-10-04 08:31:30 +0300

    task4 - commit task

--- src/main/java/ru/diasoft/spring/service/PrintServiceImp.java
+++ src/main/java/ru/diasoft/spring/service/PrintServiceImp.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class PrintServiceImp implements PrintService {
 
     public void printCommonInfo(String info) {
-        System.out.println("Common information: " + info);
+        System.out.println(info);
     }
 
     @Override
